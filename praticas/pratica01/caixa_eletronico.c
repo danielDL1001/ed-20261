3c124725638113164182ac6c09aeea3b564ecba7
Daniel Dias <danieldias1001@gmail.com>
2026-03-20 14:21:25 -0300

    Pratica01 Finalizada

--- praticas/pratica01/caixa_eletronico.c
+++ praticas/pratica01/caixa_eletronico.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
 
 float saque(float valor) {
-    if valor > 1000.0 {
-        return -1
+    if (valor > 1000.0) {
+        printf("Erro: Valor de saque excedido!");
+        return -1;
+    }
+    int resto = valor; // Caso o valor seja impar
+
+    int n200 = resto / 200;
+    resto %= 200;
+
+    int n100 = resto / 100;
+    resto %= 100;
+
+    int n50 = resto / 50;
+    resto %= 50;
+
+    int n20 = resto / 20;
+    resto %= 20;
+
+    int n10 = resto / 10;
+    resto %= 10;
+
+    int n5 = resto / 5;
+    resto %= 5;
+
+    int n2 = resto / 2;
+    resto %= 2;
+
+    printf("Valor do saque: %d\n", valor);
+    printf("Notas de 200: %d\n", n200);
+    printf("Notas de 100: %d\n", n100);
+    printf("Notas de 50: %d\n", n50);
+    printf("Notas de 20: %d\n", n20);
+    printf("Notas de 10: %d\n", n10);
+    printf("Notas de 5: %d\n", n5);
+    printf("Notas de 2: %d\n", n2);
+
+    if (resto == 0) {
+        printf("Valor restante não pode ser sacado!\n");
+    }
+    else {
+        printf("Valor restante a ser sacado: %d\n", resto);
     }
 
 }
 int main()
 {
-    saque(200);
-    saque(100);
-    saque(50);
-    saque(20);
-    saque(10);
-    saque(5);
-    saque(2);
+    saque(230);
 
     return 0;
 }

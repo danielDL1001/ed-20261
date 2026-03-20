7e64d92e90b35d807974f29894987e0da4a7c21e
Daniel Dias <danieldias1001@gmail.com>
2026-03-20 15:47:32 -0300

    Finalizei a pratica02

--- praticas/pratica02/exibe_matriz.c
+++ praticas/pratica02/exibe_matriz.c
@@ -1,53 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int matriz1(FILE *arquivo) { // Por ter um laço, essa opção é mais rápida do que a matriz2 (Tempo de execução: 0,000016 segundos)
+int matriz1lac() { // Matriz com apenas um laço. Mais lenta (Tempo de execução: 0.0001510000 segundos)
     int matriz[10][10];
     for (int k = 0; k < 100; k++) { 
         int i = k / 10;
         int j = k % 10;
         matriz[i][j] = i * 10 + j;
-        fprintf(arquivo, "%d ", matriz[i][j]);
+        printf("%d ", matriz[i][j]);
         if(j == 9) {
-            fprintf(arquivo, "\n");
+            printf("\n");
         }
     }
     return 0;
 }
 
-int matriz2(FILE *arquivo) { // Por ter dois laços aninhados, essa opção é mais lenta do que a matriz1 (Tempo de execução: 0,000028 segundos)
+int matriz2lac() { // Matriz dois laços aninhados. Mais rápida (Tempo de execução: 0.0000570000 segundos)
     int matriz[10][10];
     for (int i = 0; i < 10; i++) { 
         for (int j = 0; j < 10; j++) {
             matriz[i][j] = i * 10 + j;
-            fprintf(arquivo, "%d ", matriz[i][j]);
+            printf("%d ", matriz[i][j]);
         }
-        fprintf(arquivo, "\n");
+        printf("\n");
     }
     return 0;
 }
 
 int main() {
-    FILE *arquivo = fopen("exibe_matriz.txt", "w+");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
-        return 1;
-    }
-    int opcao;
-    printf("Qual matriz deseja exibir?\n");
-    printf("1 - Matriz com um laço\n");
-    printf("2 - Matriz com dois laços aninhados\n");
-    if (scanf("%d", &opcao) != 1) {
-        printf("Entrada inválida.\n");
-        return 1;
-    }
-    if (opcao == 1) {
-        matriz1(arquivo);
-    } else if (opcao == 2) {
-        matriz2(arquivo);
-    } else {
-        printf("Opção inválida.\n");
-    }
-    fclose(arquivo);
+    clock_t tempo_inicial, tempo_final;
+    double duracao;
+    tempo_inicial = clock();
+    matriz1lac();
+    tempo_final = clock();
+    printf("\n");
+    duracao = (double)(tempo_final - tempo_inicial)/ CLOCKS_PER_SEC;
+    printf("O tempo de execucao foi %.10f\n", duracao);
+    printf("\n");
+    tempo_inicial = clock();
+    matriz2lac();
+    tempo_final = clock();
+    printf("\n");
+    duracao = (double)(tempo_final - tempo_inicial)/ CLOCKS_PER_SEC;
+    printf("O tempo de execucao foi %.10f\n", duracao);
 
     return 0;
 }

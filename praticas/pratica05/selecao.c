bb11c950d283e79754a495087e3f758b492e5135
danielDL1001 <danieldias1001@email.com>
2026-04-12 12:24:34 -0300

    Arrumando as pastas

---
+++ praticas/pratica05/selecao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "selecao.h"
+
+int linear_search(int *array, size_t size, int target) {
+    printf("Hello, World!\n");
+    return 0;
+}
+
+int quick_select(int *array, size_t size, int k) {
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ praticas/pratica05/selecao.h
@@ -0,0 +1,8 @@
+#ifndef SELECAO_H
+#define SELECAO_H
+
+
+int linear_search(int *array, size_t size, int target);
+int quick_select(int *array, size_t size, int k);
+
+#endif

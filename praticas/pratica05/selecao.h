b767abe1df9b026e36d0a97d990436c0953e60f0
danielDL1001 <danieldias1001@email.com>
2026-04-19 13:19:16 -0300

    Pratica05 implementando logica de pesquisa

--- praticas/pratica05/selecao.h
+++ praticas/pratica05/selecao.h
@@ -1,8 +1,8 @@
 #ifndef SELECAO_H
 #define SELECAO_H
 
-
-int linear_search(int *array, size_t size, int target);
-int quick_select(int *array, size_t size, int k);
+int linear_search(int v[], int n, int valor);
+int quick_select(int v[], int n, int valor);
+int busca_sequencial(int v[], int n, int valor);
 
 #endif

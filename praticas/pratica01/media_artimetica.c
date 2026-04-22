b432eb85f32e5efa27d96ab316cb6217ee7fa5d1
danielDL1001 <danieldias1001@email.com>
2026-04-22 15:36:08 -0300

    Finalizei e modifiquei pratica01

--- praticas/pratica01/media_artimetica.c
+++ praticas/pratica01/media_artimetica.c
@@ -1,34 +1,32 @@
 #include <stdio.h>
+#define TAMANHO 8
 
 float media_result(int n, float soma)
 {
     return soma / n;
 }
 
-int main() { 
-    float array[100] = {
-    3.2, 7.8, 1.1, 9.4, 0.5, 6.7, 2.3, 8.9, 4.6, 5.0,
-    7.1, 0.9, 3.8, 6.2, 1.7, 9.0, 2.5, 8.3, 4.1, 5.6,
-    0.2, 7.4, 3.3, 6.8, 1.9, 9.7, 2.0, 8.6, 4.9, 5.3,
-    7.6, 0.8, 3.5, 6.1, 1.4, 9.2, 2.7, 8.0, 4.3, 5.9,
-    0.6, 7.3, 3.0, 6.5, 1.8, 9.6, 2.2, 8.7, 4.8, 5.1,
-    7.9, 0.4, 3.6, 6.9, 1.2, 9.3, 2.6, 8.4, 4.0, 5.7,
-    0.1, 7.5, 3.9, 6.3, 1.6, 9.8, 2.1, 8.2, 4.7, 5.4,
-    7.0, 0.7, 3.1, 6.6, 1.3, 9.1, 2.8, 8.8, 4.2, 5.8,
-    0.3, 7.2, 3.4, 6.0, 1.5, 9.5, 2.4, 8.1, 4.4, 5.2,
-    7.7, 0.0, 3.7, 6.4, 1.0, 9.9, 2.9, 8.5, 4.5, 5.5
-    }, soma = 0.0;
-    int n = 100;
+int main() {
+    int vetor [TAMANHO];
+    for (int i = 0; i < TAMANHO; i++) 
+    {   
+        printf("Digite o elemento %d: ", i + 1);
+        scanf("%d", &vetor[i]);
+    }
+    float soma = 0;
     
-    if (n <= 0) {
+    if (TAMANHO <= 0) { // Verificação de entrada válida
         printf("Erro: A quantidade de elementos deve ser maior que zero.\n");
+        return 1;
+    }   else if (TAMANHO > 100) { // Limite de 100 elementos
+        printf("Erro: A quantidade de elementos deve ser menor ou igual a 100.\n");
         return 1;
     }
     
-    for (int i = 0; i < n; i++) {
-        soma += array[i];
+    for (int i = 0; i < TAMANHO; i++) {
+        soma += vetor[i];
     }
-    printf("A média é: %.2f\n", media_result(n, soma));
+    printf("A média é: %.2f\n", media_result(TAMANHO, soma));
 
     return 0;
 }
--- praticas/pratica01/potencia.c
+++ praticas/pratica01/potencia.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
 
-int main() { 
-    int potencia = 0;
-    float num = 8.0, resultado = 1;
-    
-    for (int i = 0; i<potencia; i++) {
+int main() {
+    float num, resultado = 1;
+    int potencia;
+
+    // Valores de teste1
+    num = 2; potencia = 3; resultado = 1;
+    for (int i = 0; i < potencia; i++) {
         resultado *= num;
     }
+    printf("1 Teste: %.2f\n", resultado);
 
-    // Arquivo de texto para salvar o resultado da potência
-    printf("O resultado da potência é: %.2f\n", resultado);
+    // Valores de teste2
+    num = 7; potencia = 6; resultado = 1;
+    for (int i = 0; i < potencia; i++) {
+        resultado *= num;
+    }
+    printf("2 Teste: %.2f\n", resultado);
     
     return 0;
 }
